46a3758f0b09aff5d21641dfc5a8371477c88442
starlight5234 <starlight5234@protonmail.ch>
2021-03-13 23:11:44 +0530

    polybar: Switch to a different style
    Change-Id: I5f4a0d353207022ff8ad85a24da0f5052a4c17ba
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar starbar &
+polybar rightbar &
+polybar leftbar &
 
 echo "Polybar launched..."
