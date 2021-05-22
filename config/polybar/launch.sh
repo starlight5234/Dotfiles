25e68636f21c7fe1946b3e7bb7a89358f88d2d81
starlight5234 <starlight5234@protonmail.ch>
2021-05-22 11:54:21 +0530

    polybar: Update
    Change-Id: Ie18ea33c1eb85077e297b27cd833cbd7149740fe
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -7,8 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar rightbar &
-polybar leftbar &
-# polybar polybar &
+#polybar rightbar &
+#polybar leftbar &
+polybar polybar &
 
 echo "Polybar launched..."
